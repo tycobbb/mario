a133c8ba199df7bd3093e713ad65210618fde74f
Ty Cobb <ty.cobb.m@gmail.com>
2021-11-08 17:21:23 -0500

    mario 2

---
+++ Assets/Mario/Core/Linear.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+/// the params for a linear equation
+[Serializable]
+public struct Linear<T> {
+    // -- props --
+    [Tooltip("a destination value")]
+    public T Value;
+
+    [Tooltip("a scale. interpretation is context-dependent.")]
+    public float Scale;
+
+    // -- lifetime --
+    /// create a new linear value
+    public Linear(T val, float scale) {
+        Value = val;
+        Scale = scale;
+    }
+
+    // -- factories --
+    /// creates a "zero" value
+    public static Linear<T> Zero {
+        get => new Linear<T>(default, 0.0f);
+    }
+}
